39b17a68d872819ef749ab1471fffe530839efb6
Maddy <polakowska.mag@gmail.com>
2022-03-14 10:51:06 +0100

    Some strategies

--- lesson-06/service-wookiee/sw.js
+++ lesson-06/service-wookiee/sw.js
@@ -48,10 +48,30 @@ self.addEventListener('fetch', async (event) => {
 
 self.addEventListener('fetch', async (event) => {
     event.respondWith(
-        caches.match(event.request).then(cachedResponse => {
-                return cachedResponse || fetch(event.request);
-        }
-    )
-    )
+        //*****CACHE FIRST
+        //     caches.match(event.request).then(cachedResponse => {
+        //             return cachedResponse || fetch(event.request);
+        //     }
+        // )
+
+        //*****NETWORK FIRST
+        fetch(event.request).catch(error => {
+                return caches.match(event.request);
+            })
+        )
+
+        //*****STALE WHILE REVALIDATE
+        // caches.match(event.request).then(cachedResponse => {
+        //         const networkFetch = fetch(event.request).then(response => {
+        //         // update the cache with a clone of the network response
+        //         caches.open("pwa-assets").then(cache => {
+        //             cache.put(event.request, response.clone());
+        //         });
+        //         });
+        //         // prioritize cached response over network
+        //         return cachedResponse || networkFetch;
+        //     }
+        // )
+        //)
 });
 
